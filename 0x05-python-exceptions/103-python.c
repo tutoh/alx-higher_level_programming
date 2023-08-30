767e9ed16e089c5ff036a2131cd2c67a8d16d630
tutoh <gicherudianagee@gmail.com>
2023-08-30 21:02:51 +0000

    good

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,14 @@
+/*
+ * File: 103-python.c
+ * Auth: tutoh
+ */
+
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+/**
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
